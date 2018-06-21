ae47797faf220a4c2978cc1c6c81820f43fb38ea
Tommy <tombricehenry@gmail.com>
2018-06-21 16:26:02 +1000

    getting back to basics

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template, jsonify
-from application.solar_analytics_scraper import SessionData, SITE_IDS
 import pendulum
 
 pendulum.set_formatter('alternative')
 app = Flask(__name__)
-app_2 = Flask(__name__)
-
-s = SessionData()
-s.login()
 
 from application import server
